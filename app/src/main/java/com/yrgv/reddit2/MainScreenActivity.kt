57796663cb0ad5267e8be80769ad6ba0985f93c5
Yedu Raghav <mailyeduraghav@gmail.com>
2021-01-08 16:11:27 -0500

    Initial commit

---
+++ app/src/main/java/com/yrgv/reddit2/MainScreenActivity.kt
@@ -0,0 +1,11 @@
+package com.yrgv.reddit2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_screen)
+    }
+}
